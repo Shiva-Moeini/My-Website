094a825258068c1565541940b41772c25951a690
Sheev <shiva.moeini20@gmail.com>
2025-02-05 13:28:13 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static export
-    basePath: "/My-Website", // Replace with your GitHub repo name
-    images: {
-      unoptimized: true, // Fixes image issues
-    },
-  };
-  
-  export default nextConfig;
-  
+  basePath: "/My-Website", // Replace with your GitHub repo name
+  images: {
+    unoptimized: true, }// Fixes image issues
+};
+
+export default nextConfig;
