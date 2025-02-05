03ce36a78b210b6b6f836a2e51142e1192edabff
Sheev <shiva.moeini20@gmail.com>
2025-02-05 13:52:19 +0100

    work on CSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export", // Enables static export
-  basePath: "/My-Website", // Replace with your GitHub repo name
-  images: {
-    unoptimized: true, }// Fixes image issues
-};
+const nextConfig = {};
 
 export default nextConfig;
